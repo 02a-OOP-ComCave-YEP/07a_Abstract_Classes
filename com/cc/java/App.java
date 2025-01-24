a96ae2bd774c2a25579143b7a90785c70fc4bd11
PiSera1 <kevinwellens24@gmail.com>
2025-01-24 10:49:19 +0100

    3. Variante Overloading

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,11 +4,33 @@ public class App {
     
     public static void main(String[] args) {
      
-       
+    Rectangle rectangle = new Rectangle(10.0, 10.0);
+    Circle circle = new Circle(5.0);
+    Triangle triangle = new Triangle(10.0, 10.0);
+    
+    output("------ Flächen ------"); 
+    output("Fläche Rechteck: " + rectangle.area());
+    output("Fläche Kreis: " + circle.area());
+    output("Fläche Dreieck: " + triangle.area());
+
+        // Geht nicht mehr... Abstraktion
+        // Shape shape = new Shape();
+
+    output("------ Differenzen ------");
+    output("Diff. r-t: " + areaDiff(rectangle, triangle));
+    output("Diff. r-t: " + areaDiff(rectangle, circle));
+
+
     }
 
-  
+    // 1. Iteration : ggf. 1000x überladen
+    private static double areaDiff(Rectangle r, Triangle t){
+        return r.area() - t.area();
+    }
 
+    private static double areaDiff(Rectangle r, Circle c){
+        return r.area() - c.area();
+    }
    
 
 
